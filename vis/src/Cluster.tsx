2aa6e9661bf732bb86d4cc9a1f467e117fe32ff5
peter@output.sk <peter@output.sk>
2023-03-05 20:07:36 +0200

    feat: restyle

--- vis/src/Cluster.tsx
+++ vis/src/Cluster.tsx
@@ -4,8 +4,17 @@ import './Cluster.css';
 
 export function Cluster({ name, group, files, properties, names }: TypeCluster) {
   const namesMarkup = Object.entries(names).sort((a, b) => b[1] - a[1]).map(([name, freq]) => (
-    <span key={name} className="name-freq">{name} ({freq}x)</span>
+    <span key={name} className="name-freq mono">{name} ({freq}x)</span>
   ))
+
+  const alsoKnownMarkup = (
+    namesMarkup.length > 1 ? (
+      <>
+        <span>Also known as</span>
+        {namesMarkup}
+      </>
+    ) : <></>
+  );
   const propertiesMarkup = properties.map(({ key, value }) => (
     <span key={key}>
       <span className="property key">{key}</span>
@@ -17,20 +26,21 @@ export function Cluster({ name, group, files, properties, names }: TypeCluster) 
   const filesMarkup = files.map(({ file, pos }) => `${file} (${pos[0]} - ${pos[1]})`).join('\n')
   return (
     <div className="cluster">
-      <div className="names">
-        <h3>Names ({namesMarkup.length})</h3>
-        <div className="pre">
-          {namesMarkup}
+      <div className="title">
+        <span>{'{}'}</span>
+        <h2 className="mono">{name}</h2>
+        <div className="row">
+          {alsoKnownMarkup}
         </div>
       </div>
       <div className="properties">
-        <h3>Properties ({properties.length})</h3>
-        <div className="pre">
+        <h3>Type properties ({properties.length})</h3>
+        <div className="pre mono">
           {propertiesMarkup}
         </div>
       </div>
       <div className="files">
-        <h3>Files ({files.length})</h3>
+        <h3>Found in {files.length} files</h3>
         <pre>
           {filesMarkup}
         </pre>
