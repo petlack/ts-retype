7ee1c787fd7423e1a6778a325e48932ba794a3ed
peter@output.sk <peter@output.sk>
2023-03-06 10:22:34 +0200

    test: fix tests

--- tests/similarity.test.ts
+++ tests/similarity.test.ts
@@ -7,27 +7,54 @@ function expectSimilarity(given: Similarity, expected: Similarity) {
 
 describe('similarity', () => {
   test('identical types with no properties', () => {
-    const given = similarity({ name: 'foo', properties: [] }, { name: 'foo', properties: [] });
+    const given = similarity(
+      { name: 'foo', properties: [], pos: [0, 0] },
+      { name: 'foo', properties: [], pos: [0, 0] },
+    );
     expectSimilarity(given, Similarity.Identical);
   });
   test('identical types with primitive properties', () => {
     const given = similarity(
-      { name: 'foo', properties: [{ key: 'bar', type: 'StringKeyword', value: 'string' }] },
-      { name: 'foo', properties: [{ key: 'bar', type: 'StringKeyword', value: 'string' }] },
+      {
+        name: 'foo',
+        pos: [0, 0],
+        properties: [{ key: 'bar', type: 'StringKeyword', value: 'string' }],
+      },
+      {
+        name: 'foo',
+        pos: [0, 0],
+        properties: [{ key: 'bar', type: 'StringKeyword', value: 'string' }],
+      },
     );
     expectSimilarity(given, Similarity.Identical);
   });
   test('types with different properties keys', () => {
     const given = similarity(
-      { name: 'foo', properties: [{ key: 'bar', type: 'StringKeyword', value: 'string' }] },
-      { name: 'foo', properties: [{ key: 'xyz', type: 'StringKeyword', value: 'string' }] },
+      {
+        name: 'foo',
+        pos: [0, 0],
+        properties: [{ key: 'bar', type: 'StringKeyword', value: 'string' }],
+      },
+      {
+        name: 'foo',
+        pos: [0, 0],
+        properties: [{ key: 'xyz', type: 'StringKeyword', value: 'string' }],
+      },
     );
     expectSimilarity(given, Similarity.Different);
   });
   test('types with different properties values', () => {
     const given = similarity(
-      { name: 'foo', properties: [{ key: 'bar', type: 'StringKeyword', value: 'string' }] },
-      { name: 'foo', properties: [{ key: 'bar', type: 'NumberKeyword', value: 'number' }] },
+      {
+        name: 'foo',
+        pos: [0, 0],
+        properties: [{ key: 'bar', type: 'StringKeyword', value: 'string' }],
+      },
+      {
+        name: 'foo',
+        pos: [0, 0],
+        properties: [{ key: 'bar', type: 'NumberKeyword', value: 'number' }],
+      },
     );
     expectSimilarity(given, Similarity.HasSimilarProperties);
   });
@@ -35,17 +62,25 @@ describe('similarity', () => {
     const given = similarity(
       {
         name: 'foo',
+        pos: [0, 0],
         properties: [
           { key: 'bar', type: 'StringKeyword', value: 'string' },
           { key: 'xyz', type: 'StringKeyword', value: 'number' },
         ],
       },
-      { name: 'foo', properties: [{ key: 'bar', type: 'NumberKeyword', value: 'string' }] },
+      {
+        name: 'foo',
+        pos: [0, 0],
+        properties: [{ key: 'bar', type: 'NumberKeyword', value: 'string' }],
+      },
     );
     expectSimilarity(given, Similarity.HasSubsetOfProperties);
   });
   test('identical types with no properties and different names', () => {
-    const given = similarity({ name: 'foo', properties: [] }, { name: 'bar', properties: [] });
+    const given = similarity(
+      { name: 'foo', pos: [0, 0], properties: [] },
+      { name: 'bar', pos: [0, 0], properties: [] },
+    );
     expectSimilarity(given, Similarity.HasIdenticalProperties);
   });
 });
