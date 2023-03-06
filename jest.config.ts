00d75985d7744452e97becea140980a849bfbdc9
peter@output.sk <peter@output.sk>
2023-03-06 23:06:55 +0200

    refactor: move files

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ const config: Config.InitialOptions = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
-  collectCoverageFrom: ['<rootDir>/src/**', '<rootDir>/tests/**', '<rootDir>/bin/**'],
+  collectCoverageFrom: ['<rootDir>/src/**', '<rootDir>/tests/**'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   coverageReporters: ['json-summary', 'lcov'],
 };
