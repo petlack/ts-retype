d43f684bd80a2fbce0b489155a665e60aa250a71
petlack <peter@output.sk>
2023-03-08 21:04:02 +0200

    docs: add examples

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -9,13 +9,13 @@ export default function App() {
           <span>TS</span><span>retype</span>
         </div>
         <div className="links">
-          <a href="/a">See on Github</a>
-          <a href="/a">See on NPM</a>
+          <a href="https://github.com/petlack/ts-retype">See on Github</a>
+          <a href="https://www.npmjs.com/package/ts-retype">See on NPM</a>
         </div>
       </header>
       <section>
         <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet</p>
+        <p>Discover duplicate TypeScript types in your codebase. </p>
       </section>
       <section>
         <h2>Install</h2>
@@ -34,9 +34,9 @@ export default function App() {
         <h2>Examples</h2>
         <p>See example reports for following projects</p>
         <ul>
-          <li><a href="./example-ts-retype.html">github.com/petlack/ts-retype</a></li>
-          <li><a href="./example-apollo-client.html">github.com/apollographql/apollo-client</a></li>
-          <li><a href="./example-apollo-server.html">github.com/apollographql/apollo-server</a></li>
+          <li><a href="./report-ts-retype.html">github.com/petlack/ts-retype</a></li>
+          <li><a href="./report-apollo-client.html">github.com/apollographql/apollo-client</a></li>
+          <li><a href="./report-apollo-server.html">github.com/apollographql/apollo-server</a></li>
         </ul>
       </section>
       <footer>
