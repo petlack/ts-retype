a0391a8c8b585c6fefc735811db3a05b22cb2205
peter@output.sk <peter@output.sk>
2023-03-27 02:24:04 +0300

    fix: refactor Bash

--- docs/src/components/Bash.tsx
+++ docs/src/components/Bash.tsx
@@ -1,4 +1,4 @@
-import { toColorTokens } from '../format';
+import { Snippet, Token, toTokenElements } from './Token';
 import { Window } from './Window';
 import { WithBash } from './WithBash';
 
@@ -7,17 +7,37 @@ export type BashProps = {
   theme: 'dark' | 'light';
 }
 
-function parseBash(code: string): string {
+function parseBash(code: string): Snippet {
   const lines = code.split('\n')
-    .map(line => line.trim().startsWith('#') ? `$com$${line}` : `$gen$${line}`);
-  return lines.join('\n');
+    .map(line => [
+      line.trim().startsWith('#') ?
+      {
+        type: 'element',
+        properties: { className: ['token', 'comment'] },
+        children: [{ type: 'text', value: line }],
+      } as Token :
+      {
+        type: 'text',
+        value: line,
+      } as Token,
+      {
+        type: 'text', value: '\n',
+      } as Token
+    ])
+    .reduce((res, item) => [...res, ...item], []);
+
+  return {
+    name: 'bash',
+    lang: 'bash',
+    code: {
+      type: 'root',
+      children: lines.slice(0, -1),
+    },
+  };
 }
 
 export function Bash({ children, theme }: BashProps) {
-  const tokens = toColorTokens(parseBash(children), theme);
-  const childrenMarkup = tokens.map(([[className], chunk], idx) => (
-    <span key={idx} className={`chunk ${className}`}>{chunk}</span>
-  ));
+  const childrenMarkup = toTokenElements(parseBash(children));
   // const childrenMarkup = children.split('\n').map((line, idx) => <span key={idx}>{`${line}\n`}</span>);
   return (
     <div className="bash">
