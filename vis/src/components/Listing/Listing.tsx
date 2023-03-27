16146fe3640727e4cf479d2d57ba9e832232f35b
peter@output.sk <peter@output.sk>
2023-03-27 10:15:17 +0300

    feat: responsive top bar

--- vis/src/components/Listing/Listing.tsx
+++ vis/src/components/Listing/Listing.tsx
@@ -14,6 +14,9 @@ export function Listing({ results, query }: ListingProps) {
     <ClusterListing clusters={results} query={query} />;
   return (
     <div className="listing">
+      <div className="info">
+        <p>Displaying results</p>
+      </div>
       {resultsMarkup}
     </div>
   );
