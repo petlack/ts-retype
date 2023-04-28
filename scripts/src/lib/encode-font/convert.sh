ed2fa8f5478b91f72ecfbda179f126a220493e0e
peter@output.sk <peter@output.sk>
2023-04-28 09:17:42 +0300

    chore: cleanup

--- scripts/src/lib/encode-font/convert.sh
+++ scripts/src/lib/encode-font/convert.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 path_to_ttf_font"
+  echo "Usage: $0 path_to_font"
   exit 1
 fi
 
-# Get the input TTF font file path
-input_ttf="$1"
+input_font="$1"
 
-# Check if the input TTF font file exists
-if [ ! -f "$input_ttf" ]; then
-  echo "Error: Input TTF font file '$input_ttf' does not exist."
+if [ ! -f "$input_font" ]; then
+  echo "Error: Input font file '$input_font' does not exist."
   exit 1
 fi
 
-cat $input_ttf | base64 -w 0 > font.b64
+cat $input_font | base64 -w 0 > font.b64
 cat font.b64
