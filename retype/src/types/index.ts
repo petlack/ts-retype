00840a45926b3509b60197e6b23920d51fb3ea95
peter@output.sk <peter@output.sk>
2023-05-01 12:11:19 +0300

    fix: retype exports

--- retype/src/types/index.ts
+++ retype/src/types/index.ts
@@ -1,5 +1,7 @@
-export type { ArrayElement } from './utils';
 export { DEFAULT_SCAN_PROPS, DEFAULT_CMD_PROPS, DEFAULT_CONFIG } from './defaults';
-export type { ReportProps, RetypeCmdProps, ScanProps } from './props';
+export { TS_RETYPE_CMD_OPTIONS } from './props';
+export type { ArrayElement } from './utils';
 export type { Metadata, ReportResult } from './report';
 export type { TypeDuplicate } from './duplicate';
+export type { Snippet, Token, TokenRoot, TokenText } from './snippet';
+export type { ReportProps, ScanProps, RetypeCmdProps } from './props';
