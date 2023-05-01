7ea392b48d5b6de85c16aa2bc1e3b024bfea01db
peter@output.sk <peter@output.sk>
2023-05-01 23:18:47 +0300

    feat: add playwright

---
+++ e2e/docs.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5174/');
+  await page.locator('#about').getByText('yarn').click();
+  await page.locator('#about').getByText('npm').click();
+  await page.locator('#about').getByText('npm', { exact: true }).click();
+  await page.locator('#about').getByText('yarn').click();
+  await page.getByText('yarn').nth(2).click();
+  await page.getByText('yarn').nth(4).click();
+  await page.locator('#usage').getByText('npx').click();
+  await page.getByText('bash').click();
+  await page.getByRole('link', { name: 'About' }).click();
+  await expect(page).toHaveURL(/.*#about/);
+  await page.getByRole('link', { name: 'Install' }).click();
+  await expect(page).toHaveURL(/.*#install/);
+  await page.getByRole('link', { name: 'Usage' }).click();
+  await expect(page).toHaveURL(/.*#usage/);
+  await page.getByRole('link', { name: 'Examples' }).click();
+  await expect(page).toHaveURL(/.*#examples/);
+});
