1c89df06e949ecad2ef842ad660174f1143f4aa1
Peter Laca <peter@laca.me>
2024-02-04 04:28:58 +0200

    cleanup

--- apps/vis/vite.config.ts
+++ apps/vis/vite.config.ts
@@ -3,20 +3,10 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 // import { visualizer } from 'rollup-plugin-visualizer';
-// import dynamicImport from 'vite-plugin-dynamic-import';
-// import json from '@rollup/plugin-json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
-    // build: {
-    //   rollupOptions: {
-    //     input: {
-    //       index: 'index.html',
-    //       data: 'src/data.json',
-    //     },
-    //   }
-    // },
     optimizeDeps: {
         include: ['react/jsx-runtime'],
     },
