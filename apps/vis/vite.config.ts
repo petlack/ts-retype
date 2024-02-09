61d9084530e0846365cb2d18f25d463f31c63150
Peter Laca <peter@laca.me>
2024-02-09 20:49:49 +0200

    feat(vis): add visualizer to build

--- apps/vis/vite.config.ts
+++ apps/vis/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { viteSingleFile } from 'vite-plugin-singlefile';
-// import { visualizer } from 'rollup-plugin-visualizer';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,9 +14,9 @@ export default defineConfig({
         react(),
         tsconfigPaths(),
         viteSingleFile(),
-        // visualizer({
-        //     emitFile: true,
-        //     filename: 'stats.html',
-        // }),
+        visualizer({
+            emitFile: true,
+            filename: 'stats.html',
+        }),
     ],
 });
