6d220b52ca0bd15b90f0f6c913df8e91daca0fa0
Peter Laca <peter@laca.me>
2024-02-09 03:43:29 +0200

    feat: uikit components

---
+++ examples/code-components/src/Inputs.tsx
@@ -0,0 +1,17 @@
+import { FC, useState } from 'react';
+import { InputNumber } from '@ts-retype/uikit';
+
+export const Inputs: FC<{ className: string }> = ({ className }) => {
+    const [value, setValue] = useState(0);
+    return <>
+        <div className={className}>
+            <label htmlFor="input-number">Input number</label>
+            <InputNumber
+                id="input-number"
+                value={value}
+                min={0}
+                onChange={setValue}
+            />
+        </div>
+    </>;
+};
