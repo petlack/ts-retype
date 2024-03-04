80f11731455e9815b0a6be89ff4982c59dd86e02
Peter Laca <peter@laca.me>
2024-03-04 08:18:07 +0200

    refactor(uikit): refactor clsx

--- packages/uikit/tsup.config.ts
+++ packages/uikit/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     entry: [
-        'src/clsx.js',
+        'src/clsx.ts',
         'src/code.ts',
         'src/hooks.ts',
         'src/icons.tsx',
