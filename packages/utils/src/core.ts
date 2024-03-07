1427126545506fa8e8555f4ebb8f664b7b046b93
Peter Laca <peter@laca.me>
2024-03-07 19:27:19 +0200

    feat(utils): add time utils

--- packages/utils/src/core.ts
+++ packages/utils/src/core.ts
@@ -4,21 +4,6 @@ export function filterEmpty<T extends object>(obj: T): Partial<T> {
     ) as Partial<T>;
 }
 
-export function formatDuration(ms: number) {
-    let sec = ms / 1000;
-    if (sec < 60) {
-        return `${sec.toFixed(2)}s`;
-    }
-    let min = Math.floor(sec / 60);
-    sec -= min * 60;
-    if (min < 60) {
-        return `${min}m ${sec.toFixed(2)}s`;
-    }
-    const hours = Math.floor(min / 60);
-    min -= hours * 60;
-    return `${hours}h ${min}m ${sec.toFixed(2)}`;
-}
-
 export function freq(list: (string | number | symbol)[]) {
     const map = list.reduce((res, item) => {
         res[item] = (res[item] || 0) + 1;
@@ -56,3 +41,5 @@ export function lines(str: string | undefined | null): string[] {
 export function round(x: number, decimals: number): number {
     return Math.round(x * 10 ** decimals) / 10 ** decimals;
 }
+
+export { formatDuration } from './time.js';
---
+++ packages/utils/src/time.ts
@@ -0,0 +1,47 @@
+export const ONE_SECOND = 1_000;
+export const ONE_MINUTE = 60 * ONE_SECOND;
+export const ONE_HOUR = 60 * ONE_MINUTE;
+export const ONE_DAY = 24 * ONE_HOUR;
+
+/**
+* Format a duration in milliseconds to a human-readable string.
+* @param ms - The duration in milliseconds.
+* @param format - The format to use.
+*   - `'digital'`: Format the duration as a digital clock. (1:45.12)
+*   - `'text'`: Format the duration as a human-readable string. (1m 45s)
+*  @returns The formatted duration.
+*/
+export function formatDuration(
+    ms: number,
+    format: 'digital' | 'text' = 'text',
+) {
+    let sec = ms / 1000;
+    if (sec < 60) {
+        switch (format) {
+        case 'digital':
+            return sec.toFixed(3);
+        case 'text':
+            return `${sec.toFixed(3)}s`;
+        }
+    }
+    let min = Math.floor(sec / 60);
+    sec -= min * 60;
+    if (min < 60) {
+        switch (format) {
+        case 'digital':
+            return `${pad(min)}:${sec.toFixed(3).padStart(6, '0')}`;
+        case 'text':
+            return `${min}m ${sec.toFixed(3)}s`;
+        }
+    }
+    const hours = Math.floor(min / 60);
+    min -= hours * 60;
+    switch (format) {
+    case 'digital':
+        return `${pad(hours)}:${pad(min)}:${sec.toFixed(3).padStart(6, '0')}`;
+    case 'text':
+        return `${hours}h ${min}m ${sec.toFixed(3)}s`;
+    }
+}
+
+const pad = (n: number) => n.toString().padStart(2, '0');
