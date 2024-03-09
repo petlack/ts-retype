4b1a248c08a1e0954ea1316e23d7f338f565ccb0
Peter Laca <peter@laca.me>
2024-03-09 19:18:59 +0200

    fix: shims warnings

--- apps/bin/tsup.config.ts
+++ apps/bin/tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     splitting: false,
     sourcemap: true,
     clean: false,
+    shims: true,
     format: ['cjs'],
     noExternal: [ /(.*)/ ],
 });
--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -12,4 +12,5 @@ export default defineConfig({
     sourcemap: true,
     clean: true,
     dts: true,
+    shims: true,
 });
