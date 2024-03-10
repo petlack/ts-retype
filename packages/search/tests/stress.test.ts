ed861f117264b77b1c55a104f537fed6518e2f84
Peter Laca <peter@laca.me>
2024-03-10 19:06:30 +0200

    fix: test

--- packages/search/tests/stress.test.ts
+++ packages/search/tests/stress.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'vitest';
 import { getAllCandidates } from '../src/parse.js';
 import { loadFile } from '../src/utils.js';
 
-describe.only('stress test', () => {
+describe('stress test', () => {
     test('binderBinaryExpressionStressJs.ts', async () => {
         const srcFile = loadFile('./tests/sources/binderBinaryExpressionStressJs.ts');
         const types = getAllCandidates(srcFile);
